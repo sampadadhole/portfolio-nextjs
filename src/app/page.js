e5ab0256c2fe38fe988767ac71afb71e374c6ba5
sampadadhole4 <dhole.s@northeastern.edu>
2023-04-25 13:16:02 -0400

    added umami

--- src/app/page.js
+++ src/app/page.js
@@ -4,24 +4,18 @@ import styles from "./page.module.css";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
-import { Inter, Barlow } from 'next/font/google'
+import { Inter, Barlow } from "next/font/google";
 import { motion } from "framer-motion";
 import { Navbar } from "@/components/Navbar";
 import Contact from "@/components/Contact";
+// import ReactGA from "react-ga4";
 
-
-const inter = Inter({ subsets: ['latin'] })
-const barlow = Barlow({ subsets: ['latin'], weight: '400' })
+const inter = Inter({ subsets: ["latin"] });
+const barlow = Barlow({ subsets: ["latin"], weight: "400" });
 export default function Home() {
   return (
-    // <div className='container mx-auto'>
-    //   <Hero />
-    //   <About />
-    //   <Projects />
-    // </div>
-
     <div className="dark:bg-ebonyClay bg-mischka">
-      <div className={barlow.className} >
+      <div className={barlow.className}>
         <Navbar />
         <Hero />
         <About />
